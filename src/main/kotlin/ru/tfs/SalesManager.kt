c3d8e584882ceff03308cb454352b0972be40665
Vera Kuzmina <verakuzmina@gmail.com>
2022-02-24 15:50:29 +0700

    add homework for lesson OOP

---
+++ src/main/kotlin/ru/tfs/SalesManager.kt
@@ -0,0 +1,25 @@
+package ru.tfs
+
+import java.math.BigDecimal
+import java.math.BigDecimal.ZERO
+
+class SalesManager(private val salesPerMonthAmount: BigDecimal, private val pricePerSale: BigDecimal) : Employee {
+
+    private var bankAccount: BigDecimal = ZERO
+
+    override fun payWages(sum: BigDecimal) {
+        bankAccount += sum
+        println("SalesManager's salary paid: $sum, total amount: $bankAccount")
+    }
+
+    override fun getContractSum(monthCount: Int): BigDecimal {
+        return salesPerMonthAmount * pricePerSale * monthCount.toBigDecimal()
+    }
+
+    fun calculateMonthlyBonus(salesAmountLimit: BigDecimal, bonusAmount: BigDecimal) {
+        if (salesPerMonthAmount >= salesAmountLimit) {
+            bankAccount += bonusAmount
+            println("SalesManager's bonus paid: $bonusAmount, total amount: $bankAccount")
+        }
+    }
+}
