391bf0c9f4726058c173541a3338cc88520c8e80
Vera Kuzmina <verakuzmina@gmail.com>
2022-05-08 18:08:04 +0700

    add homework for lesson 5

---
+++ src/main/kotlin/ru/tfs/Car.kt
@@ -0,0 +1,9 @@
+package ru.tfs
+
+data class Car(
+    val brand: String,
+    val model: String,
+    val bodyType: String,
+    val price: Int,
+    val fuelConsumption: Double
+)
---
+++ src/main/kotlin/ru/tfs/CarService.kt
@@ -0,0 +1,35 @@
+package ru.tfs
+
+class CarService(private val translator: Translator) {
+
+    private val exchangeRate: Int = 10
+
+    fun translateAndConvertPrice(cars: List<Car>): List<Car> {
+        return cars.asSequence()
+            .map { it.translateAndConvertPrice() }
+            .sortedBy { it.price }
+            .toList()
+    }
+
+    fun groupByBodyType(cars: List<Car>): Map<String, List<Car>> {
+        return cars.asSequence()
+            .groupBy { it.bodyType }
+    }
+
+    fun filterByPriceLower(cars: List<Car>, maxPrice: Int): List<String> {
+        return cars.asSequence()
+            .filter { it.price <= maxPrice }
+            .map { "${it.brand} ${it.model}" }
+            .take(3)
+            .toList()
+    }
+
+    private fun Car.translateAndConvertPrice(): Car {
+        return copy(
+            brand = translator.translate(brand),
+            model = translator.translate(model),
+            bodyType = translator.translate(bodyType),
+            price = price * exchangeRate
+        )
+    }
+}
